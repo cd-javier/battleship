b015733b79b3359d648c4fc0ed1578cff8846c34
Javier Quiroga <contact@javierquiroga.com>
2025-03-28 16:29:13 +0000

    test: add unit tests for Cell class functionality
    - Added tests for untouched cells: content, placement restrictions, and
    hit status
    - Verified behavior of hit cells: hit status, double-hit prevention, and
    ship interaction
    - Covered edge cases such as placing multiple ships and hitting sunk
    ships

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable quotes */
 /* eslint-disable no-undef */
-import { Ship } from './battleship';
+import { Ship, Cell } from './battleship';
 
 describe('Ship class', () => {
   describe('Length 1', () => {
@@ -85,6 +85,55 @@ describe('Ship class', () => {
       ship.hit();
       ship.hit();
       expect(() => ship.hit()).toThrow();
+    });
+  });
+});
+
+describe('Cell class', () => {
+  let cell;
+  beforeEach(() => {
+    cell = new Cell();
+  });
+
+  describe('Untouched cell', () => {
+    test('Content is undefined', () => {
+      expect(cell.content).toBeUndefined();
+    });
+
+    test('Content is ship', () => {
+      const ship = new Ship(1);
+      cell.place(ship);
+      expect(cell.content).toBe(ship);
+    });
+
+    test("Doesn't allow to place a ship twice", () => {
+      const ship = new Ship(1);
+      const ship2 = new Ship(1);
+      cell.place(ship);
+      expect(() => cell.place(ship2)).toThrow();
+    });
+
+    test('isHit is false', () => {
+      expect(cell.isHit).toBe(false);
+    });
+  });
+
+  describe('Hit cell', () => {
+    test('isHit is true', () => {
+      cell.hit();
+      expect(cell.isHit).toBe(true);
+    });
+
+    test("Doesn't allow to be hit twice", () => {
+      cell.hit();
+      expect(() => cell.hit()).toThrow();
+    });
+
+    test('When the cell contains a ship, the ship has been hit', () => {
+      const ship = new Ship(1);
+      cell.place(ship);
+      cell.hit();
+      expect(cell.content.sunk).toBe(true);
     });
   });
 });
