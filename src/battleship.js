ecb7b34183ed5f5f88448448c71beb8ffe8a0966
Javier Quiroga <contact@javierquiroga.com>
2025-03-28 16:31:11 +0000

    feat: implement Cell class
    - Added constructor to initialize cell content and hit status
    - Implemented place method to set content and prevent duplicate
    placements
    - Added hit method to track hits and interact with Ship instances if
    present
    - Included error handling for invalid operations (e.g., hitting twice or
    overwriting content)

--- src/battleship.js
+++ src/battleship.js
@@ -19,4 +19,34 @@ class Ship {
   }
 }
 
-export { Ship };
+class Cell {
+  constructor() {
+    this.content = undefined;
+    this.isHit = false;
+  }
+
+  place(obj) {
+    if (this.content) {
+      throw new Error(
+        "This cell already has content that can't be overwritten"
+      );
+    }
+    this.content = obj;
+  }
+
+  hit() {
+    if (this.isHit) {
+      throw new Error(
+        "This cell has already been hit and it can't be hit again"
+      );
+    }
+
+    this.isHit = true;
+
+    if (this.content instanceof Ship) {
+      this.content.hit();
+    }
+  }
+}
+
+export { Ship, Cell };
