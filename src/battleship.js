7e537ee8deac0e2c650f81200b73d7c069a3f393
Javier Quiroga <contact@javierquiroga.com>
2025-03-28 15:54:48 +0000

    feat: implement Ship class
    - Added constructor to initialize ship length, hits, and sunk status
    - Implemented hit method to track hits and throw an error if already sunk
    - Added isSunk method to determine if the ship has sunk based on hits
    - Exported Ship class for use in Battleship project

---
+++ src/battleship.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    if (this.sunk) {
+      throw new Error("This ship has already been sank and can't be hit again");
+    }
+    this.hits++;
+    this.isSunk();
+  }
+
+  isSunk() {
+    this.sunk = this.hits >= this.length;
+  }
+}
+
+export { Ship };
