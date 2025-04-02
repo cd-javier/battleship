788c8a512d04fd91f34319ff1c61cf417f50d65e
Javier Quiroga <contact@javierquiroga.com>
2025-04-02 17:23:38 +0100

    feat: implement interactive gameplay mechanics
    - Added Selector module to streamline DOM element selection
    - Enhanced renderGameboard function with dataset attributes for cell
    coordinates
    - Introduced displayMessage function to relay dynamic game messages to
    players
    - Developed turn-based gameplay logic for Player 1 and Player 2
    - Implemented event listeners for player attacks and automated CPU turns
    - Updated startGame function to reset and initialize gameplay seamlessly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import './styles.css';
 
 import { Player, CPUPlayer } from './battleship';
@@ -7,19 +8,25 @@ const Selector = (function () {
   const actions = document.getElementById('actions');
   const playerGameboard = document.getElementById('player-gameboard');
   const opponentGameboard = document.getElementById('opponent-gameboard');
+  const startBtn = document.getElementById('start');
 
-  return { display, actions, playerGameboard, opponentGameboard };
+  return { display, actions, playerGameboard, opponentGameboard, startBtn };
 })();
 
-function displayGameboard(player, isOpponent) {
+function renderGameboard(player, isOpponent) {
   const board = player.gameboard.board;
   const target = isOpponent
     ? Selector.opponentGameboard
     : Selector.playerGameboard;
 
+  target.innerHTML = '';
+
   board.flat().forEach((cell) => {
     const singleCell = document.createElement('div');
     singleCell.classList.add('gameboard-cell');
+    const [cellY, cellX] = cell.coords;
+    singleCell.dataset.y = cellY;
+    singleCell.dataset.x = cellX;
 
     if (!isOpponent && cell.content) {
       singleCell.classList.add('ship');
@@ -40,36 +47,89 @@ function displayGameboard(player, isOpponent) {
   });
 }
 
-const player1 = new Player();
-const player2 = new Player();
+function displayMessage(message) {
+  Selector.display.textContent = message;
+}
 
-player1.placeShip(1, 2);
-player1.placeShip(3, 5, false);
-player1.placeShip(3, 8, false);
-player1.placeShip(9, 1, true);
-player1.placeShip(3, 1);
-player1.placeShip(7, 7);
+function playerOneTurn() {
+  function playerOneEventListener(e) {
+    const targetCell = e.target;
 
-player2.placeShip(1, 2);
-player2.placeShip(3, 5, false);
-player2.placeShip(3, 8, false);
-player2.placeShip(9, 1, true);
-player2.placeShip(3, 1);
-player2.placeShip(7, 7);
+    const y = targetCell.dataset.y;
+    const x = targetCell.dataset.x;
 
-player1.gameboard.receiveAttack(0, 0);
-player1.gameboard.receiveAttack(1, 2);
-player1.gameboard.receiveAttack(1, 3);
-player1.gameboard.receiveAttack(1, 4);
-player1.gameboard.receiveAttack(1, 5);
-player1.gameboard.receiveAttack(1, 6);
+    let attack;
 
-player2.gameboard.receiveAttack(0, 0);
-player2.gameboard.receiveAttack(1, 2);
-player2.gameboard.receiveAttack(1, 3);
-player2.gameboard.receiveAttack(1, 4);
-player2.gameboard.receiveAttack(1, 5);
-player2.gameboard.receiveAttack(1, 6);
+    if (
+      !targetCell.classList.contains('hit') &&
+      !targetCell.classList.contains('miss')
+    ) {
+      attack = player2.gameboard.receiveAttack(y, x);
+      renderGameboard(player2, true);
+      Selector.opponentGameboard.removeEventListener(
+        'click',
+        playerOneEventListener
+      );
 
-displayGameboard(player1, false);
-displayGameboard(player2, true);
+      if (attack) {
+        if (player2.gameboard.hasUnsunkShips()) {
+          playerOneTurn();
+        } else {
+          displayMessage('Game over - Player 1 Wins!');
+        }
+      } else {
+        playerTwoTurn();
+      }
+    } else {
+      displayMessage("You can't his the same cell twice");
+    }
+  }
+
+  Selector.opponentGameboard.addEventListener('click', playerOneEventListener);
+}
+
+function playerTwoTurn() {
+  const y = Math.floor(Math.random() * 10);
+  const x = Math.floor(Math.random() * 10);
+  let attack;
+
+  try {
+    attack = player1.gameboard.receiveAttack(y, x);
+  } catch {
+    playerTwoTurn();
+  }
+
+  renderGameboard(player1, false);
+
+  if (attack) {
+    if (player2.gameboard.hasUnsunkShips()) {
+      playerTwoTurn();
+    } else {
+      displayMessage('Game over - Player 2 Wins!');
+    }
+  } else {
+    playerOneTurn();
+  }
+}
+
+function startGame() {
+  Selector.startBtn.textContent = 'RESTART';
+
+  player1 = new Player();
+  player2 = new CPUPlayer();
+
+  player1.placeShip(1, 2);
+  player1.placeShip(3, 5, false);
+  player1.placeShip(3, 8, false);
+  player1.placeShip(8, 1, true);
+  player1.placeShip(3, 1);
+  player1.placeShip(7, 7);
+
+  renderGameboard(player1, false);
+  renderGameboard(player2, true);
+  playerOneTurn();
+}
+
+let player1, player2;
+
+Selector.startBtn.addEventListener('click', startGame);
